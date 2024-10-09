df1b2308df474cb9a6fe8807b229cf1f61c3e72b
koike <koike.ryotaro.58a@st.kyoto-u.ac.jp>
2024-10-09 20:19:49 +0900

    changed Server.c so that getaddrinfo() gives address info based on command line argments

--- Server.c
+++ Server.c
@@ -4,13 +4,16 @@
 #include <string.h> //memset()
 #include <unistd.h> //read()
 #include <stdlib.h>
+#include <netdb.h>
 
 /*
-USAGE : Server(char servAddress, int servPort)
+USAGE : Server(node, servPort)
     This program starts up a server that communicate through sockets.
+    The argments node and servPort will be directly passed to getaddrinfo() 
+    as the 1st and 2nd argment respectively.
     The argment servPort is the port this program will wait on, 
-    and servPort needs to be an integer between 0 and 65535
-EXAMPLE : Server(8080)
+    and servPort needs to be an integer between 0 and 65535.
+EXAMPLE : Server(localhost, 8080)
 */
 
 
@@ -19,6 +22,8 @@ int main(int argc, char* argv[]){
     int clientSock; //client socket file discriptor
     struct sockaddr_in serverSockAddr; //server internet socket address
     struct sockaddr_in clientSockAddr; //client internet socket address
+    struct addrinfo hints;
+    struct addrinfo *results, *res;
     char buf[1024];
     int rval;
     long serverPort;
@@ -29,6 +34,7 @@ int main(int argc, char* argv[]){
         exit(1);
     }
 
+/*
     char *endptr;
     serverPort = strtol(argv[2], &endptr, 10); // will be casted to unsigned short in the call of htons()
     //if some part of argment is invalid or if port is not in the range of unsigned short(0~65535)
@@ -36,22 +42,39 @@ int main(int argc, char* argv[]){
         printf("Invalid port number: %s\n", argv[1]);
         exit(1);
     }
-    
+*/
 
-    serverSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSock == -1){
-        perror("opening stream socket");
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;      // IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    int statusOfGetaddrinfo = getaddrinfo(argv[1], argv[2], &hints, &results);
+    if (statusOfGetaddrinfo != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(statusOfGetaddrinfo));
         exit(1);
     }
 
-    memset(&serverSockAddr, 0, sizeof(serverSockAddr));
-    serverSockAddr.sin_family = AF_INET;
-    serverSockAddr.sin_addr.s_addr = INADDR_ANY;
-    serverSockAddr.sin_port = htons(serverPort);
+    /*  getaddrinfo() returns a list of address structures.
+        Try each address until we successfully bind(2).
+        If socket(2) (or bind(2)) fails, we (close the socket
+        and) try the next address. */
+    int statusOfBind;
+    for (res = results; res != NULL; res = res->ai_next){
+        serverSock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (serverSock == -1){ continue; }
 
-    int statusOfBind = bind(serverSock, (struct sockaddr *) &serverSockAddr, sizeof(serverSockAddr)); //第２引数のキャストはなぜ必要?
-    if (statusOfBind != 0){
-        perror("binding stream socket");
+        statusOfBind = bind(serverSock, res->ai_addr, res->ai_addrlen);
+        if (statusOfBind == 0){ break; }
+
+        close(serverSock); 
+    }
+    if (res == NULL) { //No address succeeded
+        perror("binding");
         exit(1);
     }
 
@@ -70,11 +93,11 @@ int main(int argc, char* argv[]){
         } else { 
             do{
                 memset(buf, 0, sizeof(buf));
-                if ((rval = read(clientSock, buf, sizeof(buf))) == -1)
+                if ((numOfBytesRead = read(clientSock, buf, sizeof(buf))) == -1)
                     perror("reading stream message");
-                if (rval == 0)
+                if (numOfBytesRead == 0)
                     printf("Ending connection\n");
-            } while (rval > 0);
+            } while (numOfBytesRead > 0);
 
             close(clientSock);
         }
